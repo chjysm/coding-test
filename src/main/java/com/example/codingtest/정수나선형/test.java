4c24cd1f12e33862e6bf640ee306856f2e645d5d
chjysm <chjysm2@naver.com>
2024-04-10 20:36:46 +0900

    정수를 나선형으로 배치하기 문제 https://school.programmers.co.kr/learn/courses/30/lessons/181832

--- src/main/java/com/example/codingtest/정수나선형/test.java
+++ src/main/java/com/example/codingtest/정수나선형/test.java
@@ -1,2 +1,91 @@
-package com.example.codingtest.정수나선형;public class test {
+package com.example.codingtest.정수나선형;
+
+public class test {
+    private static int[][] target;
+    private static int currentNum;
+    private static int input;
+
+    public static void main(String[] args) {
+        solution(4);
+
+        for(int i = 0; i<input; i++){
+            for(int j=0;j<input;j++){
+                System.out.println(target[i][j]);
+            }
+        }
+    }
+
+    public static int[][] solution(int n) {
+        currentNum = 0;
+        target = new int[n][n];
+        input = n;
+        
+        xAxis(0,0,true);
+        return target;
+    }
+
+    private static void xAxis(int yIndex, int xIndex, boolean isIncrement ){
+        int lastIndex = 0;
+        if( isIncrement ){
+            for( int i = xIndex; ; i++ ){
+                if( i > input - 1
+                        ||target[yIndex][i] != 0){
+                    lastIndex = i-1;
+                    break;
+                }
+                target[yIndex][i] = ++currentNum;
+            }
+
+            if(currentNum >= input * input ){
+                return;
+            }
+
+            yAxis( yIndex + 1, lastIndex, true);
+        } else {
+            for( int i = xIndex; ; i-- ){
+                if( i < 0
+                        || target[yIndex][i] != 0){
+                    lastIndex = i+1;
+                    break;
+                }
+                target[yIndex][i] = ++currentNum;
+            }
+            if(currentNum >= input * input ){
+                return;
+            }
+
+            yAxis( yIndex - 1, lastIndex, false);
+        }
+    }
+    private static void yAxis(int yIndex, int xIndex, boolean isIncrement ){
+        int lastIndex = 0;
+        if( isIncrement ){
+            for(int i = yIndex; ; i++ ){
+                if( i > input - 1
+                        || target[i][xIndex] != 0){
+                    lastIndex = i-1;
+                    break;
+                }
+
+                target[i][xIndex] = ++currentNum;
+            }
+            if(currentNum >= input * input ){
+                return;
+            }
+            xAxis( lastIndex, xIndex - 1, false);
+        } else {
+            for(int i = yIndex; ; i-- ){
+                if( i < 0
+                        || target[i][xIndex] != 0){
+                    lastIndex = i+1;
+                    break;
+                }
+                target[i][xIndex] = ++currentNum;
+            }
+            if(currentNum >= input * input ){
+                return;
+            }
+            xAxis( lastIndex, xIndex + 1, true);
+        }
+    }
 }
